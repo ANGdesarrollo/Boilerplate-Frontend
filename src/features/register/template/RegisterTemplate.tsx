89c95b59f1f294060e4356023fc16ddab99f9402
ANGDesarrollo <alexisgraff123@gmail.com>
2023-09-27 22:09:18 -0300

    feat/ag/hook form

--- src/features/register/template/RegisterTemplate.tsx
+++ src/features/register/template/RegisterTemplate.tsx
@@ -1,10 +1,25 @@
+'use client';
 import React from 'react';
 import { InputForm } from '@/features/shared/molecules/InputForm';
+import { useForm } from 'react-hook-form';
 
+export type RegistrationFormFields = {
+  username: string;
+};
 export const RegisterTemplate = () => {
+  const {
+    handleSubmit,
+    formState: { errors }
+  } = useForm<RegistrationFormFields>();
+
+  const onSubmit = handleSubmit((data) => {
+    const lala = data;
+    const lele = errors;
+  });
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <InputForm id="username" name="username" label="username" placeholder="Username" type="text"/>
+      <button className="mx-2  bg-blue text-sm  shadow-lg shadow-purple" type={'submit'}>Submit</button>
     </form>
   );
 };
