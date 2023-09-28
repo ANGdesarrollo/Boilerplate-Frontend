6637a85e9daf32c92b2027ec3de5a25ea0f30ba5
ANGDesarrollo <alexisgraff123@gmail.com>
2023-09-28 20:40:12 -0300

    feat/ag/test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { RegisterTemplate } from '@/features/register/template/RegisterTemplate';
 
 const IndexPage = () => {
-  return (<RegisterTemplate/>);
+  return <RegisterTemplate />;
 };
 
 export default IndexPage;
--- src/features/register/template/RegisterTemplate.tsx
+++ src/features/register/template/RegisterTemplate.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import { useForm } from 'react-hook-form';
 
 export type RegistrationFormFields = {
   username: string;
@@ -8,66 +7,13 @@ export type RegistrationFormFields = {
   confirmPassword: string;
 };
 export const RegisterTemplate = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors }
-  } = useForm<RegistrationFormFields>();
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   watch,
+  //   formState: { errors }
+  // } = useForm<RegistrationFormFields>();
 
-  const onSubmit = (data: RegistrationFormFields) => console.log(data);
-  return (
-    <form action="" onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="username">Ingresa tu email y dale a subscribirse</label>
-      <input type="email"
-        {...register('username', {
-          required: {
-            value: true,
-            message: 'correo requerido'
-          },
-          pattern: {
-            value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-            message: 'correo no valido'
-          }
-        })}
-      />{
-        errors.username && <span>{errors.username.message}</span>
-      }
-
-      <label htmlFor="password">Password</label>
-      <input type="password"
-        {...register('password', {
-          required:{
-            value: true,
-            message: 'Campo requerido'
-          }, minLength: {
-            value: 4,
-            message: 'El minimo de caracteres es 4'
-          },
-          maxLength: {
-            value: 10,
-            message: 'El maximo de caracteres es 10'
-          }
-        })}
-      />{
-        errors.password && <span>{errors.password.message}</span>
-      }
-
-      <label htmlFor="confirmpassword">confirmar password</label>
-      <input type="password"
-        {...register('confirmPassword', { required:{
-          value: true,
-          message: 'campo requerido'
-        },
-        validate:(value) => {
-          return value === watch('password') || 'los password no coinciden';
-        }
-        })}
-      />{
-        errors.confirmPassword &&  <span>{errors.confirmPassword.message}</span>
-      }
-
-      <button>enviar</button>
-    </form>
-  );
+  // const onSubmit = (data: RegistrationFormFields) => console.log(data);
+  return <div></div>
 };
